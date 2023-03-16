13c4f2b2c762778b6ee088cb6d0974100470268a
Nnenna Nkata <nellyroberto23@gmail.com>
2023-03-16 13:20:48 -0700

    c print out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 int main(void)
 	{
-		string print ="Programming is like building a multilingual puzzle";
+		char print ="Programming is like building a multilingual puzzle";
 		puts(print);
     
 		return 0;

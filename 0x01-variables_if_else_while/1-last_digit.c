b850763ab030446aecf05341f140d991dc669754
Nnenna Nkata <nellyroberto23@gmail.com>
2023-03-17 16:36:25 -0700

    if else to print lastdigit of input n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,20 @@
 int main(void)
 {
 	int n;
+	int lastdigit;
+
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int lastn = n % 10;
+	lastdigit = n % 10;
 
-	if (lastn > 5)
-		printf("Last digit of %n is %d and is greater than 5", n, lastn);
-	else if (lastn == 0)
-		printf("Last digit of %d is %d and is 0", n, lastn);
-	else if (lastn < 6 && lastn != 0)
-		printf("Last digit %d is %d and is less than 6 and not 0", n, lastn);
+	if (lastdigit > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, lastdigit);
+	else if (lastdigit == 0)
+		printf("Last digit of %d is %d and is 0", n, lastdigit);
+	else if (lastdigit < 6 && lastdigit != 0)
+		printf("Last digit %d is %d and is less than 6 and not 0", n, lastdigit);
 	printf("/n");
 	return (0);
 }

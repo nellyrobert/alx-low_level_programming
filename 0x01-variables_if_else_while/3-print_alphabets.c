66d4a478015d957072202048771d60d45c17e60a
Nnenna Nkata <nellyroberto23@gmail.com>
2023-03-17 17:24:58 -0700

    print a to z and A to Z using for loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - main code startes here
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	for (alph = 'A'; alph <= 'Z'; alph++)
+		putchar(alph);
+	putchar('\n');
+	return (0);
+}

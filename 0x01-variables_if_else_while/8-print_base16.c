d7f536526cd590cfb727c93f3e0a280f3a5baca3
Nnenna Nkata <nellyroberto23@gmail.com>
2023-03-17 18:38:05 -0700

    print num and alphabet

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 		putchar(num + '0');
-	for (num_alph = 10; num_alph <= 15; num_alph++)
+	for (num_alph = 'a'; num_alph <= 'f'; num_alph++)
 		putchar(num_alph);
 	putchar('\n');
 	return (0);

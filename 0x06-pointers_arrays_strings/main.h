4ac7e341390e749d436f2aaf0c997964f6f8784c
Nnenna Nkata <nellyroberto23@gmail.com>
2023-03-30 11:52:51 -0700

    function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,10 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *_strncpy(char *dest, char *src, int n);
-
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 
 #endif
